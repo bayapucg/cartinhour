eea5c2f1b322c55999317c937981d09710d378f9
bayapucg <bayapu.edu004@gmail.com>
2017-06-13 05:35:05 -0400

    add validations for add products

--- application/views/seller/products/add.php
+++ application/views/seller/products/add.php
@@ -1,3 +1,7 @@
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/bootstrapValidator.css"/>
+    <script src="<?php echo base_url(); ?>assets/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/dist/js/bootstrapValidator.js"></script>
 
 <div class="content-wrapper mar_t_con" >
 <section class="content-header">
@@ -40,10 +44,10 @@
             <!--<header class="panel-heading"> Basic Forms </header>-->
 			<div><?php echo $this->session->flashdata('message');?></div>
             <div class="panel-body">
-              <form action="<?php echo base_url(); ?>seller/products/insert/<?php echo $this->uri->segment(4); ?>/<?php echo $this->uri->segment(5); ?>" method="post" enctype="multipart/form-data" onSubmit="return scvalidateof();this.js_enabled.value=1;return true;">
+              <form name="addprodustc" id="addprodustc" action="<?php echo base_url(); ?>seller/products/insert/<?php echo $this->uri->segment(4); ?>/<?php echo $this->uri->segment(5); ?>" method="post" enctype="multipart/form-data">
                 <div class="form-group nopaddingRight col-md-6 san-lg">
                   <label for="exampleInputEmail1">Select Category</label>
-                 <select class="form-control" id="category_id" name="category_id">
+                 <select class="form-control " id="category_id" name="category_id">
                     <option value="">Select Category</option>
 					
 					 <?php foreach($sub_cat_data as $single_cat_data){ ?>
@@ -53,7 +57,6 @@
 					 <?php }?>
                   </select>
 				 
-				 <span style="color: 	;position: absolute;top: 0;right: 20px;" id="errorcategory"></span>
                 </div>
 				
 				
@@ -65,7 +68,6 @@
                     <option value="<?php echo $subcat_data->subcategory_id; ?>"><?php echo $subcat_data->subcategory_name; ?></option>
                     <?php } ?>
                   </select>
-				   <span style="color:red; position: absolute;top: 0;right: 20px;" id="errorsubcategory"></span>
                 </div>
 				<div class="form-group nopaddingRight col-md-6 san-lg">
                   <label for="exampleInputPassword1">Select subitem</label>
@@ -73,34 +75,29 @@
                     <option value="">Select Subitem</option>
           
                   </select>
-           <span style="color:red; position: absolute;top: 0;right: 20px;" id="errorsubitem"></span>
                 </div>
 				
                 
 				 <div class="form-group nopaddingRight col-md-6 san-lg">
                   <label for="exampleInputEmail1">Item Name</label>
                   <input class="form-control" placeholder="Item Name" type="text" id="item_name" name="item_name">
-				  <span style="color:red ;position: absolute;top: 0;right: 20px;" id="erroritemname"></span>
            </div>
 
            <div class="form-group nopaddingRight col-md-6 san-lg">
                   <label for="exampleInputEmail1">Item Code</label>
                   <input class="form-control" placeholder="Item Code" type="text" id="item_code" name="item_code">
-          <span style="color:red;position: absolute;top: 0;right: 20px;" id="erroritemcode"></span>
            </div>
 
 				 <div class="form-group nopaddingRight col-md-6 san-lg">
                   <label for="exampleInputEmail1">Item Quantity</label>
                   <input class="form-control" placeholder="Item Quantity" type="text" id="item_quantity" name="item_quantity">
-				  <span style="color:red;position: absolute;top: 0;right: 20px;" id="errorquantity"></span>
                 </div>
 				 <div class="form-group nopaddingRight col-md-6 san-lg">
                   <label for="exampleInputEmail1">Item Charges</label>
                   <input class="form-control" placeholder="Item Charges" type="text" name="item_cost" id="item_cost">
-				  <span style="color:red;position: absolute;top: 0;right: 20px;" id="errorcost"></span>
                 </div>
                 
-                <div class="clearfix"></div>
+               
 				<div class="form-group nopaddingRight col-md-6 san-lg">
                   <label for="exampleInputPassword1">Status</label>
                   <select class="form-control" id="item_status" name="item_status">
@@ -108,18 +105,16 @@
                     <option value="1">Available</option>
                     <option value="2">Unavailable</option>
                   </select>
-				  <span style="color:red;position: absolute;top: 0;right: 20px;" id="errorstatus"></span>
+                </div>
+				
+                <div class="form-group nopaddingRight col-md-6 san-lg">
+                  <label for="exampleInputEmail1">Description</label>
+                  <textarea  placeholder="Item Description" class="form-control" rows="3" id="item_description" name="item_description"></textarea>
                 </div>
 				<div class="form-group nopaddingRight col-md-6 san-lg">
                   <label for="exampleInputFile">Image</label>
                   <input type="file" name="picture" id="picture">
 				  
-				   <span style="color:red;position: absolute;top: 0;right: 20px;" id="errorpicture"></span>
-                </div>
-				<div class="clearfix"></div>
-                <div class="form-group nopaddingRight col-md-6 san-lg">
-                  <label for="exampleInputEmail1">Description</label>
-                  <textarea  placeholder="Item Description" class="form-control" rows="3" id="item_description" name="item_description"></textarea>
                 </div>
                
 				
@@ -129,6 +124,7 @@
                 <button type="submit" class="btn btn-danger" onclick="window.location='<?php echo base_url(); ?>seller/products';return false;">Cancel</button>
 				</div>
               </form>
+			   
             </div>
           </section>
         </div>
@@ -141,7 +137,7 @@
   </div>
   <!--main content end--> 
 
-       
+     
 		
   
   <script type="text/javascript">
@@ -192,210 +188,88 @@
     });
     });
     </script>   
-		
-<script>
 	
-	function scvalidateof()
-
-{
-
-var category_id=document.getElementById('category_id');
-
-var subcategory_id=document.getElementById('subcategory_id');
-
-var item_name=document.getElementById('item_name');
-
-var item_code=document.getElementById('item_code');
-
-var item_quantity=document.getElementById('item_quantity');
-
-var picture=document.getElementById('picture');
-
-
-var item_status=document.getElementById('item_status');
-var item_cost=document.getElementById('item_cost');
-
-
-var patterns = /[0-9]|\./;
-//var phone_validation = /^(?=.*?[1-9])[0-9()-+]+$/ ;
-
-//var name=/^[^-\s][a-zA-Z_\s-]+$/;
-
-
-//alert(category_id.value);
-
-if(category_id.value==""){
-
-document.getElementById('errorcategory').innerHTML="Please Select Category";
-
-$("#category_id").focus();
-
-return false;	
-
-}	
-
-else{
-
-	document.getElementById('errorcategory').innerHTML="";
-
-}
-
-if(subcategory_id.value==""){
-
-document.getElementById('errorsubcategory').innerHTML="Please Select Subcategory";
-
-$("#subcategory_id").focus();
-
-return false;	
-
-}	
-
-else{
-
-	document.getElementById('errorsubcategory').innerHTML="";
-
-}
-
-if(item_name.value==""){
-
-document.getElementById('erroritemname').innerHTML="Please Enter Item Name";
-
-$("#item_name").focus();
-
-return false;	
-
-}	
-
-else{
-
-	document.getElementById('erroritemname').innerHTML="";
-
-}
-
-
-if(item_code.value==""){
-
-document.getElementById('erroritemcode').innerHTML="Please Enter Item Code";
-
-$("#item_code").focus();
-
-return false;	
-
-}	
-
-else{
-
-	document.getElementById('erroritemcode').innerHTML="";
-
-}
-
-if(item_quantity.value !=""){
-	
-	
-if (!patterns.test(item_quantity.value)) {
-
-document.getElementById('errorquantity').innerHTML="Item Quantity accepts Numbers only";
-
-$("#item_quantity").focus();
-
-return false;
-
-}
-
-else{
-
-	document.getElementById('errorquantity').innerHTML="";
-
-}	
-	
-}
-	
-else{
-document.getElementById('errorquantity').innerHTML="Please Enter Item Quantity";
-
-$("#item_quantity").focus();
-
-return false;	
-
-}		
-
-
-
-
-
-
-
-
-
-
-
-
-
-if(picture.value==""){
-
-document.getElementById('errorpicture').innerHTML="Please Choose Product Image";
-
-$("#picture").focus();
-
-return false;	
-
-}	
-
-else{
-
-	document.getElementById('errorpicture').innerHTML="";
-
-}
-
-
-if(item_status.value==""){
-
-document.getElementById('errorstatus').innerHTML="Please Select Status";
-
-$("#item_status").focus();
-
-return false;	
-
-}	
-
-else{
-
-	document.getElementById('errorstatus').innerHTML="";
-
-}
-
-
-if(item_cost.value !=""){
-	
-	
-if (!patterns.test(item_cost.value)) {
-
-document.getElementById('errorcost').innerHTML="Cost accepts Numbers only";
-
-$("#item_cost").focus();
-
-return false;
-
-}
-
-else{
-
-	document.getElementById('errorcost').innerHTML="";
-
-}	
-	
-}
-	
-else{
-document.getElementById('errorcost').innerHTML="Please Enter Item Cost";
-
-$("#item_cost").focus();
-
-return false;	
-
-}		
-
-
-
-}
+	<script type="text/javascript">
+$(document).ready(function() {
+    $('#addprodustc').bootstrapValidator({
+       
+        fields: {
+            category_id: {
+               validators: {
+					notEmpty: {
+						message: 'Please select a Category'
+					}
+				}
+            },
+			subcategory_id: {
+               validators: {
+					notEmpty: {
+						message: 'Please select a Subcategory'
+					}
+				}
+            },
+            item_name: {
+              validators: {
+					notEmpty: {
+						message: 'Item Name is required'
+					},
+                   regexp: {
+					regexp: /^[a-zA-Z0-9. ]+$/,
+					message: ' Item Name can only consist of alphanumaric, space and dot'
+					}
+                }
+            },
+			item_code: {
+              validators: {
+					notEmpty: {
+						message: 'Item Code is required'
+					},
+                   regexp: {
+					regexp: /^[a-zA-Z0-9. ]+$/,
+					message: ' Item Code can only consist of alphanumaric, space and dot'
+					}
+                }
+            },
+			item_quantity: {
+              validators: {
+					notEmpty: {
+						message: 'Item Quantity is required'
+					},
+                   regexp: {
+					regexp: /^[0-9]+$/,
+					message: ' Item Quantity can only consist of digits'
+					}
+                }
+            },
+			item_cost: {
+              validators: {
+					notEmpty: {
+						message: 'Item Cost is required'
+					},
+                   regexp: {
+					regexp: /^[a-zA-Z0-9. ]+$/,
+					message: ' Item Cost can only consist of alphanumaric, space and dot'
+					}
+                }
+            },
+			picture: {
+				validators: {
+					   
+					regexp: {
+					regexp: /\.(jpe?g|png|gif)$/i,
+					message: 'Uploaded file is not a valid image. Only JPG, PNG and GIF files are allowed'
+					}
+				}
+			},
+			item_status: {
+               validators: {
+					notEmpty: {
+						message: 'Please select a Status'
+					}
+				}
+            }
+        }
+    });
+});
 </script>
+		
