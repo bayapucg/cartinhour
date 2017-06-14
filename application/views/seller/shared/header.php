95699f4d02beb3971a947cd5594f5aa8b9e4f6b4
bayapucg <bayapu.edu004@gmail.com>
2017-06-14 01:39:47 -0400

    dashboard link purpose

--- application/views/seller/shared/header.php
+++ application/views/seller/shared/header.php
@@ -1,74 +1,4 @@
-<!-- 
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
  
-  <div class="navigation_main" >
-    <nav class="navbar navbar-inverse hm_nav" style="position: fixed;top:0px;z-index:1024;width:100%; ">
-      <div class="">
-        <div class="navbar-header logo_style" >
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-      <a class="navbar-brand" href="#">
-      <img  src="<?php echo base_url(); ?>assets/seller/images/logo.png" class="img-responsive" /></a>
-        </div>
-        <div class="collapse navbar-collapse" id="myNavbar">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="<?php echo base_url();?>seller/dashboard">Home</a></li>
-            
-            <li><a href="<?php echo base_url();?>seller/aboutus">About Us</a></li>
-           <li><a href="<?php echo base_url();?>seller/faqs">FAQ's</a></li>
-            <li><a href="#">Help</a></li>
-            <li><a href="<?php echo base_url();?>seller/contactus">Contact Us</a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right sm_hide">
-            
-            <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Admin Notification</a>
-              <ul class="dropdown-menu notify-drop">
-                <div class="drop-content">
-                  <?php foreach($sellernotify as $cat_data){ ?>
-                  <li>
-                    <div class="col-md-3 col-sm-3 col-xs-3">
-                      <div class="notify-img"><img src="http://placehold.it/45x45" alt=""></div>
-                    </div>
-                    <div class="col-md-9 col-sm-9 col-xs-9 pd-l0"><a href=""><?php  echo $cat_data->message; ?> </a> <a href="" class="rIcon"><i class="fa fa-dot-circle-o"></i></a> 
-                    </div>
-                  </li>
-                  <?php } ?>
-                </div>                
-              </ul>
-            </li>
-            <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Seller Notification </a>
-              <ul class="dropdown-menu notify-drop">
-                <div class="drop-content">
-                  <?php foreach($ordersdata as $order_data){ ?>
-                  <li>
-                    <div class="col-md-3 col-sm-3 col-xs-3">
-                      <div class="notify-img">&nbsp;</div>
-                    </div>
-                    <div class="col-md-9 col-sm-9 col-xs-9 pd-l0"><a href="">Product:<?php  echo $order_data->product_name; ?> </a><br><a href="">Customer Name:<?php  echo $order_data->customer_name; ?> <a href="" class="rIcon"><i class="fa fa-dot-circle-o"></i></a>
-                      
-                    </div>
-                  </li>
-                  <?php } ?>
-                </div>                
-              </ul>
-            </li>
-            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> <?php echo ucfirst($this->session->userdata('seller_name'));    ?></a>
-      <ul class="dropdown-menu extended logout">
-            <div class="log-arrow-up"></div>
-            
-      <li> <a href="<?php echo base_url() ; ?>seller/dashboard/change_password"><i class="fa fa-lock" aria-hidden="true"></i> Change Password</a> </li>
-            <li> <a href="<?php echo base_url() ; ?>seller/login/logout"><i class="fa fa-sign-out" aria-hidden="true"></i> Log Out</a> </li>          
-          </ul>
-      
-      </li>
-          </ul>
-          
-        </div>
-      </div>
-    </nav>
-  </div>
- navigation end here -->  
 <style>
 .navbar-nav>li>a{
 	padding:0px;
@@ -88,7 +18,7 @@
         <!-- Site wrapper -->
         
             <header class="main-header hm_nav" style="position: fixed;top:0px;width:100%; ">
-                <a href="http://localhost/cartinhour/trunk/seller/dashboard" class="logo"> <!-- Logo -->
+                <a href="<?php echo base_url('seller/dashboard');?>" class="logo"> <!-- Logo -->
                     <span class="logo-mini">
                         <!--<b>A</b>H-admin-->
                         <img src="<?php echo base_url(); ?>assets/seller/dist/img/mini-logo.png" alt="CIH">
@@ -109,7 +39,7 @@
 								<li class="pad_l50">
 								<ul class="nav navbar-nav pad_li">  
 								
-								<li class="active"><a href="<?php echo base_url();?>seller/dashboard">Home</a></li>
+								<li class="active"><a href="<?php echo base_url('seller/dashboard');?>">Home</a></li>
 								
 								<!--<li><a href="<?php echo base_url();?>seller/aboutus">About Us</a></li>-->
 							   <li><a href="<?php echo base_url();?>seller/faqs">FAQ's</a></li>
@@ -164,10 +94,6 @@
       <!-- Modal content-->
      <div class="modal-content"  >
    
-        <!--<div class="modal-header " style="background-color:#006a99;color:#fff;padding:10px;">
-            <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title"> Enquiry form</h4>
-        </div>-->
         <div class="modal-body" style="padding:0px;margin-top:-22px;">
 		<span type="button" class="close " data-dismiss="modal" style="position:absolute;top:12px;right:12px">&times;</span>
 		<img style="width:100%;" class="img-responsive" src="<?php echo base_url();?>assets/seller_login/images/price_hide.png" />
@@ -177,216 +103,7 @@
 			<span style="background:#006a99;padding:5px 10px ;border-radius:5px;font-size:18px;margin-left: 85px;cursor: pointer;" class="" data-dismiss="modal"  data-toggle="modal"
 			data-target="#myModal_sel_mod_en">Enquiry form</span>
 		</div>
-      <!--<div class="container">
-	  
-    <div class="row">
-        <div class="col-xs-12 col-md-3">
-            <div class="panel panel-primary">
-                <div class="panel-heading">
-                    <h3 class="panel-title">
-                        Bronze</h3>
-                </div>
-                <div class="panel-body">
-                    <div class="the-price">
-                        <h1>
-                            <i class="fa fa-inr" aria-hidden="true"></i>10<span class="subscript">/mo</span></h1>
-                        <small>1 month FREE trial</small>
-                    </div>
-                    <table class="table">
-                        <tr>
-                            <td>
-                                1 Account
-                            </td>
-                        </tr>
-                        <tr class="active">
-                            <td>
-                                1 Project
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                100K API Access
-                            </td>
-                        </tr>
-                        <tr class="active">
-                            <td>
-                                100MB Storage
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Custom Cloud Services
-                            </td>
-                        </tr>
-                        <tr class="active">
-                            <td>
-                                Weekly Reports
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-                <div class="panel-footer">
-                    <a href="http://www.jquery2dotnet.com" class="btn btn-success" role="button">Sign Up</a>
-                    1 month FREE trial</div>
-            </div>
-        </div>
-        <div class="col-xs-12 col-md-3">
-            <div class="panel panel-success">
-                <div class="cnrflash">
-                    <div class="cnrflash-inner">
-                        <span class="cnrflash-label">MOST
-                            <br>
-                            POPULR</span>
-                    </div>
-                </div>
-                <div class="panel-heading">
-                    <h3 class="panel-title">
-                        Silver</h3>
-                </div>
-                <div class="panel-body">
-                    <div class="the-price">
-                        <h1>
-                           <i class="fa fa-inr" aria-hidden="true"></i>20<span class="subscript">/mo</span></h1>
-                        <small>1 month FREE trial</small>
-                    </div>
-                    <table class="table">
-                        <tr>
-                            <td>
-                                2 Account
-                            </td>
-                        </tr>
-                        <tr class="active">
-                            <td>
-                                5 Project
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                100K API Access
-                            </td>
-                        </tr>
-                        <tr class="active">
-                            <td>
-                                200MB Storage
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Custom Cloud Services
-                            </td>
-                        </tr>
-                        <tr class="active">
-                            <td>
-                                Weekly Reports
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-                <div class="panel-footer">
-                    <a href="http://www.jquery2dotnet.com" class="btn btn-success" role="button">Sign Up</a>
-                    1 month FREE trial</div>
-            </div>
-        </div>
-        <div class="col-xs-12 col-md-3">
-            <div class="panel panel-info">
-                <div class="panel-heading">
-                    <h3 class="panel-title">
-                        Gold</h3>
-                </div>
-                <div class="panel-body">
-                    <div class="the-price">
-                        <h1>
-                           <i class="fa fa-inr" aria-hidden="true"></i>35<span class="subscript">/mo</span></h1>
-                        <small>1 month FREE trial</small>
-                    </div>
-                    <table class="table">
-                        <tr>
-                            <td>
-                                5 Account
-                            </td>
-                        </tr>
-                        <tr class="active">
-                            <td>
-                                20 Project
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                300K API Access
-                            </td>
-                        </tr>
-                        <tr class="active">
-                            <td>
-                                500MB Storage
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Custom Cloud Services
-                            </td>
-                        </tr>
-                        <tr class="active">
-                            <td>
-                                Weekly Reports
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-                <div class="panel-footer">
-                    <a href="http://www.jquery2dotnet.com" class="btn btn-success" role="button">Sign Up</a> 1 month FREE trial</div>
-            </div>
-        </div>
-    <div class="col-xs-12 col-md-3">
-            <div class="panel panel-info">
-                <div class="panel-heading">
-                    <h3 class="panel-title">
-                        Gold</h3>
-                </div>
-                <div class="panel-body">
-                    <div class="the-price">
-                        <h1>
-                           <i class="fa fa-inr" aria-hidden="true"></i>35<span class="subscript">/mo</span></h1>
-                        <small>1 month FREE trial</small>
-                    </div>
-                    <table class="table">
-                        <tr>
-                            <td>
-                                5 Account
-                            </td>
-                        </tr>
-                        <tr class="active">
-                            <td>
-                                20 Project
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                300K API Access
-                            </td>
-                        </tr>
-                        <tr class="active">
-                            <td>
-                                500MB Storage
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Custom Cloud Services
-                            </td>
-                        </tr>
-                        <tr class="active">
-                            <td>
-                                Weekly Reports
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-                <div class="panel-footer">
-                    <a href="http://www.jquery2dotnet.com" class="btn btn-success" role="button">Sign Up</a> 1 month FREE trial</div>
-            </div>
-        </div>
-    
-    </div>-->
+     
 </div>
         </div>
         
