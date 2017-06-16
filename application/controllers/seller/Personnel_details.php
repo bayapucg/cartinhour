862823db1f04c99a558ad9dc70cb4fd161a0af3f
bayapucg <bayapu.edu004@gmail.com>
2017-06-16 06:45:51 -0400

    personal details purpose

--- application/controllers/seller/Personnel_details.php
+++ application/controllers/seller/Personnel_details.php
@@ -9,8 +9,10 @@ class Personnel_details extends Admin_Controller {
 	public function __construct() {
 		parent::__construct();
 		
-		//$this->load->model('seller/products_model');
+		$this->load->model('seller/products_model');
 		$this->load->model('seller/Personnel_details_model');
+		$this->load->helper(array('url', 'html'));
+		$this->load->library('session');
        // $this->load->library('pagination');
 		
  	}
@@ -18,10 +20,12 @@ class Personnel_details extends Admin_Controller {
 	public function index()
 	{
 			   
-	   $data['sellerlocationdata'] = $this->Personnel_details_model->getlocations();
-	   $data['partsellersd'] = $this->Personnel_details_model->getsellersd();
-	   $data['partsellerpd'] = $this->Personnel_details_model->getsellerpd();
-	   $data['partsellerbd'] = $this->Personnel_details_model->getsellerbd();		
+	   $sid= $this->session->userdata('seller_id');
+	   $data['getcat'] = $this->products_model->getcatdata();
+	   $data['seller_categorudetails'] = $this->Personnel_details_model->get_store_category_detail($sid);
+	   $data['seller_storedetails'] = $this->Personnel_details_model->get_all_storedetail($sid);
+	   
+		//echo '<pre>';print_r($data);exit;	   
 		$this->template->write_view('content', 'seller/personneldetails/index', $data);
 		$this->template->render();
 
@@ -116,27 +120,146 @@ if($res)
 public function updatebd()
 {
 	
-	
-	 $data = array(
-	
-	
-	'seller_name' => $this->input->post('seller_name'),
-	'seller_email' => $this->input->post('seller_email'),
-	'seller_address' => $this->input->post('seller_address'),
-	
+	$post = $this->input->post();
+	$sid = $this->session->userdata('seller_id');
+	//ECHO '<pre>';print_r($post);exit;
+	$data = array(
+	'seller_name' => $post['seller_name'],
+	'seller_email' =>  $post['seller_email'],
+	'seller_address' =>  $post['seller_address'],
 	);
-
-
-$res=$this->Personnel_details_model->updatebd($data);
-if($res)
+	$result=$this->Personnel_details_model->updatebd($sid,$data);
+	if(count($result)>0)
 		{
-		 $this->prepare_flashmessage("Basic Details are Updated Successfully..", 0);
+		$this->prepare_flashmessage("Basic Details are Updated Successfully..", 0);
+		$this->session->set_flashdata('updatemessage',"Basic Details are Updated Successfully!");
 		return redirect('seller/Personnel_details');
-			}
-			else
+		}else
 			{
-				$this->prepare_flashmessage("Failed to Insert..", 1);
-				return redirect('seller/Personnel_details');
+			$this->prepare_flashmessage("Failed to Insert..", 1);
+			return redirect('seller/Personnel_details');
+			}	
+	
+}
+
+public function seller_storedetails()
+	{  
+		$post=$this->input->post();
+		echo '<pre>';print_r($_FILES);
+					$seller_upload_file=$this->Personnel_details_model->get_upload_file($this->session->userdata('seller_id'));
+		echo '<pre>';print_r($seller_upload_file);
+			if($_FILES['timimages']['name']!=''){
+			$tinimg=$_FILES['timimages']['name'];
+			move_uploaded_file($_FILES['timimages']['tmp_name'], "assets/sellerfile/" . $_FILES['timimages']['name']);
+
+			}else{
+			$tinimg=$seller_upload_file['tinvatimage'];
+			}
+
+			if($_FILES['tanimages']['name']!=''){
+			$tanimg=$_FILES['tanimages']['name'];
+			move_uploaded_file($_FILES['tanimages']['tmp_name'], "assets/sellerfile/" . $_FILES['tanimages']['name']);
+
+			}else{
+			$tanimg=$seller_upload_file['tanimage'];
+			}
+			if($_FILES['cstimag']['name']!=''){
+			$cetimg=$_FILES['cstimag']['name'];
+			move_uploaded_file($_FILES['cstimag']['tmp_name'], "assets/sellerfile/" . $_FILES['cstimag']['name']);
+
+			}else{
+			$cetimg=$seller_upload_file['cstimage'];
+			}
+
+		//echo '<pre>';print_r($post);
+			$data = array(
+			'store_name' => $post['storename'], 
+			'addrees1' => $post['address1'],    
+			'addrees2' => $post['address2'],    
+			'pin_code' => $post['pincode'],    
+			'other_shops'  =>$post['other_shops'],
+			'other_shops_location'  =>$post['other_shops_location'],
+			'deliveryes'  =>$post['deliveryes'],
+			'weblink'  =>$post['weblink'],
+			'tin_vat'  =>$post['tin'],
+			'tinvatimage'  =>$tinimg,
+			'tan'  =>$post['tan'],
+			'tanimage'  =>$tanimg,
+			'cst'  =>$post['cst'],
+			'cstimage'  =>$cetimg,
+			'gstin'  =>$post['gstin'],
+			 'created_at'  => date('Y-m-d H:i:s'),
+			);
+			//echo '<pre>';print_r($data);exit;
+			$addstoredetails=$this->Personnel_details_model->storedetails_adding($this->session->userdata('seller_id'),$data);
+			
+			if(count($addstoredetails)>0)
+			{
+			$this->session->set_flashdata('storeupdatemessage','Store details updated');
+			return redirect('seller/Personnel_details');
+			}
+			//echo '<pre>';print_r($data);exit;
+
+
+    }
+	
+	
+	public function seller_categories(){
+			$post=$this->input->post();
+			//echo'<pre>';print_r($post);exit;
+			$result = array_unique($post['seller_cat']);
+			
+			$catresult=$this->Personnel_details_model->get_old_seller_categories($this->session->userdata('seller_id'));
+			foreach($catresult as $delcats){
+				
+				$this->Personnel_details_model->delet_get_old_seller_categories($delcats['seller_cat_id']);
+			}
+			
+			echo'<pre>';print_r($result);//exit;
+			foreach($result as $subcats){
+				
+			$data = array(
+			'seller_id' => $this->session->userdata('seller_id'),
+			'seller_category_id'=> $subcats,
+			'created_at'=> date('Y-m-d h:i:s'),
+			'updated_at'=>  date('Y-m-d h:i:s'),
+			);
+			if($subcats!=''){
+				$res=$this->Personnel_details_model->insertseller_cat($data);
+			}
+			}
+			
+			if(count($res)>0)
+			{
+			$this->session->set_flashdata('tab','2');
+			$this->session->set_flashdata('catupdatemessage','Category details updated');
+			return redirect('seller/Personnel_details');
+			}
+		
+	}
+	
+	
+public function personal_details_updatebd()
+{
+	
+	$post = $this->input->post();
+	$sid = $this->session->userdata('seller_id');
+	//ECHO '<pre>';print_r($post);exit;
+	$data = array(
+	'seller_bank_account' => $post['bank_account'],
+	'seller_pan_card' =>  $post['pan_card'],
+	'seller_adhar' =>  $post['seller_adhar'],
+	);
+	$result=$this->Personnel_details_model->updatebd($sid,$data);
+	if(count($result)>0)
+		{
+		$this->prepare_flashmessage("Personal Details are Updated Successfully..", 0);
+		$this->session->set_flashdata('perupdatemessage',"Personal Details are Updated Successfully!");
+		return redirect('seller/Personnel_details');
+		}else
+			{
+			$this->prepare_flashmessage("Failed to Insert..", 1);
+			return redirect('seller/Personnel_details');
 			}	
 	
 }
